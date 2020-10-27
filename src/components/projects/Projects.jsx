5db157a2ef42abfb6f49a002d208c3ff5593f98d
vivianP <vivianctvpham@gmail.com>
2020-10-27 12:07:55 -0700

    Updated new version of projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import hive from "../../myFiles/hiveLogo.jpg";
 import scrubs from "../../myFiles/scrubs2.jpg";
-import reactcalc from "../../myFiles/react-calculator.png";
 import notwelcome from "../../myFiles/NOT WELCOME.png";
+import winkbywynter from "../../myFiles/winkbywynter.JPG";
 import "./project.css";
-import ScrollAnimation from "react-animate-on-scroll";
 import LinkIcon from "@material-ui/icons/Link";
-// import FadeInLeftSection from "./FadeInLeftSection";
-// import FadeInRightSection from "./FadeInRightSection";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+Aos.init({ duration: 1000 });
 
 class Projects extends React.Component {
   render() {
@@ -18,228 +18,120 @@ class Projects extends React.Component {
             <div className="section-title">
               <h2>Projects</h2>
             </div>
-            <ScrollAnimation animateIn="fadeInUp">
-              <div className="row">
-                <div className="col-md-3">
-                  <div className="pContainer">
-                    <img src={notwelcome} alt="Not Welcome" duration={3} />
-                    <div className="pOverlay">
-                      <div className="description">
-                        Worked with a team to develop and deploy an exclusive
-                        social network platform that focuses on automobiles,
-                        utilizing React Hooks and Firebase SDK in order to
-                        operate with Firestore NoSQL database.
-                      </div>
-                      <div id="tech" className="techUsed">
-                        <ul className="techList">
-                          <li className="techCheck">
-                            <i className="fa fa-check" />
-                            React.js
-                          </li>
-                          <li className="techCheck">
-                            <i className="fa fa-check" />
-                            Firebase/Firestore NoSQL database
-                          </li>
-                        </ul>
-                      </div>
-                      <a href="https://notwelcome.com/" title="Not Welcome">
-                        <LinkIcon />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-3">
-                  <div className="pContainer">
-                    <img src={scrubs} alt="Scrubs Data" duration={3} />
-                    <div className="pOverlay">
-                      <div className="description">
-                        Scrubs Data was founded to improve provider directories
-                        by making it easier for doctors to update their
-                        information, health insurance companies to save money
-                        while improving accuracy, and enabling patients to more
-                        confidently select a health care practitioner. Built and
-                        implemented a highly scalable application for the
-                        company as a short term contract.
-                      </div>
-                      <div id="tech" className="techUsed">
-                        <ul className="techList">
-                          <li className="techCheck">
-                            <i className="fa fa-check" />
-                            React.js
-                          </li>
-                          <li className="techCheck">
-                            <i className="fa fa-check" />
-                            C#/.NET
-                          </li>
-                          <li className="techCheck">
-                            <i className="fa fa-check" />
-                            SQL/SQL Server
-                          </li>
-                        </ul>
-                      </div>
-                      <a href="https://notwelcome.com/" title="Not Welcome">
-                        <LinkIcon />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-3">
-                  <div className="pContainer">
-                    <img src={reactcalc} alt="React Calculator" duration={3} />
-                    <div className="pOverlay">
-                      <div className="description">
-                        Side project. Built and implemented a simple
-                        Apple-inspired Calculator from scratch.
-                      </div>
-                      <div id="tech" className="techUsed">
-                        <ul className="techList">
-                          <li className="techCheck">
-                            <i className="fa fa-check" />
-                            React.js
-                          </li>
-                        </ul>
-                      </div>
-                      <a
-                        href="https://github.com/vivianctvpham/react-calc"
-                        title="React Calculator"
-                      >
-                        <LinkIcon />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                {/* <div className="portfolio-items">
-                <div className="col-md-4 col-md-4 col-lg-4">
-                  <FadeInRightSection className="portfolio-item">
-                    <img
-                      src={notwelcome}
-                      className="img-responsive"
-                      alt="Not Welcome"
-                    />
-                  </FadeInRightSection>
-                </div>
-                <FadeInLeftSection>
-                  <div className="col-md-7">
-                    <h4>
-                      NotWelcome LLC
-                      <a href="https://notwelcome.com/" title="NotWelcome LLC">
-                        <i className="fa fa-external-link-square rightLink"></i>
-                      </a>
-                    </h4>
+            <div className="row" data-aos="fade-up">
+              {/* <ScrollAnimation animateIn={FadeInUp} duration={3} delay={2}> */}
+              <div className="col-md-4">
+                <div className="service-container">
+                  <img src={winkbywynter} alt="Wink by Wynter" />
+                  <div className="service-overlay">
                     <div className="description">
-                      Worked with a team to develop and deploy an exclusive
-                      social network platform that focuses on automobiles,
-                      utilizing React Hooks and Firebase SDK in order to operate
-                      with Firestore NoSQL database.
-                    </div>
-                    <div id="tech" className="techUsed">
-                      <ul>
-                        <li>
-                          <i className="fa fa-check" />
-                          React.js
-                        </li>
-                        <li>
-                          <i className="fa fa-check" />
-                          Firebase/Firestore NoSQL database
-                        </li>
-                      </ul>
+                      <div class="header">
+                        <h5 class="card-title">Wink by Wynter</h5>
+                      </div>
+                      <div className="card-body">
+                        <p className="service-text">
+                          <span>* IN PROGRESS * </span> <br />
+                          Designed and implemented a beauty services platform
+                          for a small owned business.
+                        </p>
+                        <div id="tech" className="techUsed">
+                          <ul className="techList">
+                            <li className="techCheck">
+                              <i className="fa fa-check" />
+                              React.js
+                            </li>
+                          </ul>
+                        </div>
+                        <a
+                          href="https://winkbywynter.com/"
+                          title="Wink by Wynter"
+                        >
+                          <LinkIcon />
+                        </a>
+                      </div>
                     </div>
                   </div>
-                </FadeInLeftSection>
-              </div> */}
-              </div>
-            </ScrollAnimation>
-            {/* <div className="row">
-              <div className="portfolio-items">
-                <div className="col-md-4 col-md-4 col-lg-4">
-                  <FadeInLeftSection className="portfolio-item">
-                    <img
-                      src={scrubs}
-                      className="img-responsive"
-                      alt="Scrubs Data"
-                    />
-                  </FadeInLeftSection>
                 </div>
-                <FadeInRightSection>
-                  <div className="col-md-7">
-                    <h4>
-                      Scrubs Data
-                      <a
-                        href="https://scrubsdata.azurewebsites.net/"
-                        title="Scrubs Data"
-                      >
-                        <i className="fa fa-external-link-square rightLink"></i>
-                      </a>
-                    </h4>
+              </div>
+              <div className="col-md-4">
+                <div className="service-container">
+                  <img src={notwelcome} alt="Not Welcome" />
+                  <div className="service-overlay">
                     <div className="description">
-                      Scrubs Data was founded to improve provider directories by
-                      making it easier for doctors to update their information,
-                      health insurance companies to save money while improving
-                      accuracy, and enabling patients to more confidently select
-                      a health care practitioner. Built and implemented a highly
-                      scalable application for the company as a short term
-                      contract.
-                    </div>
-                    <div id="tech" className="techUsed">
-                      <ul>
-                        <li>
-                          <i className="fa fa-check" />
-                          React.js
-                        </li>
-
-                        <li>
-                          <i className="fa fa-check" />
-                          C#/.NET
-                        </li>
-
-                        <li>
-                          <i className="fa fa-check" />
-                          SQL/SQL Server
-                        </li>
-                      </ul>
+                      <div className="header">
+                        <h5 class="card-title">Not Welcome</h5>
+                      </div>
+                      <div className="card-body">
+                        <p className="service-text">
+                          Worked with a team to develop and deploy an exclusive
+                          social network platform that focuses on automobiles,
+                          utilizing React Hooks and Firebase SDK in order to
+                          operate with Firestore NoSQL database.
+                        </p>
+                        <div id="tech" className="techUsed">
+                          <ul className="techList">
+                            <li className="techCheck">
+                              <i className="fa fa-check" />
+                              React.js
+                            </li>
+                            <li className="techCheck">
+                              <i className="fa fa-check" />
+                              Firebase/Firestore NoSQL database
+                            </li>
+                          </ul>
+                        </div>
+                        <a href="https://notwelcome.com/" title="Not Welcome">
+                          <LinkIcon />
+                        </a>
+                      </div>
                     </div>
                   </div>
-                </FadeInRightSection>
+                </div>
               </div>
+              <div className="col-md-4">
+                <div className="service-container">
+                  <img src={scrubs} alt="Scrubs Data" />
+                  <div className="service-overlay">
+                    <div className="description">
+                      <div className="header">
+                        <h5 class="card-title">Scrubs Data</h5>
+                      </div>
+                      <div className="card-body">
+                        <p className="service-text">
+                          Scrubs Data was founded to improve provider
+                          directories by making it easier for doctors to update
+                          their information, health insurance companies to save
+                          money while improving accuracy, and enabling patients
+                          to more confidently select a health care practitioner.
+                          Built and implemented a highly scalable application
+                          for the company as a short term contract.
+                        </p>
+                        <div id="tech" className="techUsed">
+                          <ul className="techList">
+                            <li className="techCheck">
+                              <i className="fa fa-check" />
+                              React.js
+                            </li>
+                            <li className="techCheck">
+                              <i className="fa fa-check" />
+                              C#/.NET
+                            </li>
+                            <li className="techCheck">
+                              <i className="fa fa-check" />
+                              SQL/SQL Server
+                            </li>
+                          </ul>
+                        </div>
+                        <a href="https://notwelcome.com/" title="Not Welcome">
+                          <LinkIcon />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* </ScrollAnimation> */}
             </div>
-            <div className="row">
-              <div className="portfolio-items">
-                <div className="col-md-4 col-md-4 col-lg-4 calculatorImg">
-                  <FadeInRightSection className="portfolio-item">
-                    <img
-                      src={reactcalc}
-                      className="img-responsive"
-                      alt="React Calculator"
-                    />
-                  </FadeInRightSection>
-                </div>
-                <FadeInLeftSection>
-                  <div className="col-md-7 calculatorDesContainer ">
-                    <h4>
-                      <a
-                        href="https://github.com/vivianctvpham/react-calc"
-                        title="Project Title"
-                      >
-                        <i className="fa fa-github leftLink"></i>
-                      </a>
-                      React Calculator
-                    </h4>
-                    <div className="calcContent">
-                      Side project. Built and implemented a simple
-                      Apple-inspired Calculator from scratch.
-                    </div>
-                    <div id="tech" className="react-calc">
-                      <ul>
-                        <li>
-                          <i className="fa fa-check" />
-                          React.js
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </FadeInLeftSection>
-              </div> */}
-            {/* </div> */}
           </div>
         </div>
       </>
